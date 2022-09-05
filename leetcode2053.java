26a97cd3f3f59748f341e53ae5f6fc6f6169af0b
Eltaj <83557585+EltajGafarli@users.noreply.github.com>
2022-09-05 23:45:16 +0400

    Create leetcode2053.java

---
+++ leetcode2053.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+      HashMap<String,Integer>map = new HashMap<>();
+      for(String s : arr){
+        if(!map.containsKey(s)){
+          map.put(s,1);
+        }else{
+          map.put(s,map.get(s) + 1);
+        }
+      }
+      for(String s : arr){
+        if(map.get(s) == 1 && (--k) == 0){
+          return s;
+        }
+      }
+      return "";
+    }
+}
