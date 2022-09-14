0ff1255c59fb5f67ff7d345449a3e7917ca427d5
Eltaj <83557585+EltajGafarli@users.noreply.github.com>
2022-09-14 23:52:28 +0400

    Create leetcode696.cpp

---
+++ leetcode696.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+      int curr = 1,prev = 0,ans = 0;
+      for(int i = 1;i<s.length();i++){
+        if(s[i] == s[i - 1]) curr++;
+        else{
+          ans += min(curr,prev);
+          prev = curr;
+          curr = 1;
+        }
+      }
+      ans += min(curr,prev);
+      return ans;
+    }
+};
