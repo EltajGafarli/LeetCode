f0dbc775adab1150477149407885992cf4f40850
Eltaj <83557585+EltajGafarli@users.noreply.github.com>
2022-09-22 17:15:53 +0400

    Create leetcode1431.cpp

---
+++ leetcode1431.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+      int mx = *max_element(begin(candies), end(candies));
+      vector<bool>ans;
+      for(int i = 0; i < candies.size(); i ++){
+        if(candies[i] + extraCandies >= mx){
+          ans.push_back(true);
+        }else ans.push_back(false);
+      }
+      return ans;
+    }
+};
